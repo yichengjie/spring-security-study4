e21a4b0eaa2c0b393047178ce3a3d3ba44c5583c
易成杰 <626659321@qq.com>
2020-08-30 23:28:55 +0800

    保存

--- mini-security-browser/src/main/java/com/yicj/security/browser/MiniSecurityBrowserAutoConfiguration.java
+++ mini-security-browser/src/main/java/com/yicj/security/browser/MiniSecurityBrowserAutoConfiguration.java
@@ -1,5 +1,6 @@
 package com.yicj.security.browser;
 
+import com.yicj.security.browser.config.BrowserSecurityConfig;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
--- mini-security-browser/src/main/java/com/yicj/security/browser/config/BrowserSecurityConfig.java
+++ mini-security-browser/src/main/java/com/yicj/security/browser/config/BrowserSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.yicj.security.browser;
+package com.yicj.security.browser.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
