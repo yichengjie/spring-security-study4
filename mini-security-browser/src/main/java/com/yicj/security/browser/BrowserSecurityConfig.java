7ffb38a8d2b015d8e53fd07f2518333ad06e52a4
易成杰 <626659321@qq.com>
2020-08-30 23:28:09 +0800

    保存

---
+++ mini-security-browser/src/main/java/com/yicj/security/browser/BrowserSecurityConfig.java
@@ -0,0 +1,28 @@
+package com.yicj.security.browser;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * ClassName: BrowserSecurityConfig
+ * Description: TODO(描述)
+ * Date: 2020/8/30 23:00
+ *
+ * @author yicj(626659321 @ qq.com)
+ * 修改记录
+ * @version 产品版本信息 yyyy-mm-dd 姓名(邮箱) 修改信息
+ */
+@Configuration
+public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin() // 用表单登录来进行身份认证
+                .and()
+            .authorizeRequests() // 下面都是授权得配置
+                .anyRequest() // 任何请求
+                .authenticated()  // 都需要身份认证
+        ;
+    }
+}
