8517ac0b9f515e18c67ddf2823160e7850b9d112
易成杰 <626659321@qq.com>
2020-09-03 22:30:03 +0800

    保存

--- mini-security-authorize/src/main/java/com/yicj/security/rbac/authorize/RbacAuthorizeConfigProvider.java
+++ mini-security-authorize/src/main/java/com/yicj/security/rbac/authorize/RbacAuthorizeConfigProvider.java
@@ -1,4 +1,20 @@
 package com.yicj.security.rbac.authorize;
 
-public class RbacAuthorizeConfigProvider {
+import com.yicj.security.core.authorize.AuthorizeConfigProvider;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+import org.springframework.stereotype.Component;
+
+
+@Order
+@Component
+public class RbacAuthorizeConfigProvider implements AuthorizeConfigProvider {
+    @Override
+    public boolean config(ExpressionUrlAuthorizationConfigurer<HttpSecurity>.ExpressionInterceptUrlRegistry config) {
+        // 这里anyRequest应该最后执行所以order为Integer.MAX_VALUE, 而且整个项目应该只有一个anyRequest，
+        // 否则前面的会被后面的覆盖, 所以这里返回true
+        config.anyRequest().access("@rbacService.hasPermission(request,authentication)") ;
+        return true;
+    }
 }
