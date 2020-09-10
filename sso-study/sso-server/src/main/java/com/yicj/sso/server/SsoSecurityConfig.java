f56fdea7476728f33f078109b7ad1e472e7efe58
易成杰 <626659321@qq.com>
2020-09-10 20:17:21 +0800

    保存

--- sso-study/sso-server/src/main/java/com/yicj/sso/server/SsoSecurityConfig.java
+++ sso-study/sso-server/src/main/java/com/yicj/sso/server/SsoSecurityConfig.java
@@ -5,12 +5,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableWebSecurity(debug = true)
 public class SsoSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
